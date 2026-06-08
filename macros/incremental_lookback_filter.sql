1ce02db209cb17d61f755f8f8edeccf855ae5baa
TimHBSWFL <timokuda33@gmail.com>
2026-06-08 15:04:06 -0400

    feat: add unit test for int_daily_metrics and fix incremental macros for unit test compatibility

--- macros/incremental_lookback_filter.sql
+++ macros/incremental_lookback_filter.sql
@@ -1,5 +1,5 @@
 {% macro incremental_lookback_filter(date_column, lookback_days) %}
-    {% if is_incremental() %}
+    {% if is_incremental() and this is not none %}
     where {{ date_column }} >= (
         select dateadd(day, -{{ lookback_days }}, max({{ date_column }}))
         from {{ this }}
